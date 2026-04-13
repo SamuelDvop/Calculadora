349c09f8bc05e3544f12e63f504054591db2c670
Samuel <samueldaviduseche15@gmail.com>
2026-04-13 17:03:35 -0500

    Inicio app

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+import com.mycompany.calculadora.Controlador.Pantalla;
+
+/**
+ *
+ * @author SENA
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+       
+        Pantalla laPantalla = new Pantalla();
+        
+        laPantalla.setVisible(true);
+        
+        
+    }
+}
